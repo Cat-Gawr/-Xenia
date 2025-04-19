546c6247bccaf83902b196193ac810c47477986a
Kona  ~  2025 © <168472716+Cat-Gawr@users.noreply.github.com>
2025-04-19 16:41:16 +0200

    Create sdl_audio_system.h

---
+++ src/xenia/apu/sdl/sdl_audio_system.h
@@ -0,0 +1,31 @@
+#ifndef XENIA_APU_SDL_SDL_AUDIO_SYSTEM_H_
+#define XENIA_APU_SDL_SDL_AUDIO_SYSTEM_H_
+
+#include "xenia/apu/audio_system.h"
+
+namespace xe {
+namespace apu {
+namespace sdl {
+
+class SDLAudioSystem : public AudioSystem {
+ public:
+  explicit SDLAudioSystem(cpu::Processor* processor);
+  ~SDLAudioSystem() override;
+
+  static bool IsAvailable() { return true; }
+
+  static std::unique_ptr<AudioSystem> Create(cpu::Processor* processor);
+
+  X_RESULT CreateDriver(size_t index, xe::threading::Semaphore* semaphore,
+                        AudioDriver** out_driver) override;
+  void DestroyDriver(AudioDriver* driver) override;
+
+ protected:
+  void Initialize() override;
+};
+
+}  // namespace sdl
+}  // namespace apu
+}  // namespace xe
+
+#endif  // XENIA_APU_SDL_SDL_AUDIO_SYSTEM_H_
