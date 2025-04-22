55591774a3972231acec4012652dfab73b7bed71
Kona  ~  2025 © <168472716+Cat-Gawr@users.noreply.github.com>
2025-04-22 18:21:24 +0200

    Create null_file.h

---
+++ src/xenia/vfs/GHy/null_file.h
@@ -0,0 +1,31 @@
+#ifndef XENIA_VFS_DEVICES_NULL_FILE_H_
+#define XENIA_VFS_DEVICES_NULL_FILE_H_
+
+#include <string>
+
+#include "xenia/base/filesystem.h"
+#include "xenia/vfs/file.h"
+
+namespace xe {
+namespace vfs {
+
+class NullEntry;
+
+class NullFile : public File {
+ public:
+  NullFile(uint32_t file_access, NullEntry* entry);
+  ~NullFile() override;
+
+  void Destroy() override;
+
+  X_STATUS ReadSync(void* buffer, size_t buffer_length, size_t byte_offset,
+                    size_t* out_bytes_read) override;
+  X_STATUS WriteSync(const void* buffer, size_t buffer_length,
+                     size_t byte_offset, size_t* out_bytes_written) override;
+  X_STATUS SetLength(size_t length) override;
+};
+
+}  // namespace vfs
+}  // namespace xe
+
+#endif  // XENIA_VFS_DEVICES_NULL_FILE_H_
