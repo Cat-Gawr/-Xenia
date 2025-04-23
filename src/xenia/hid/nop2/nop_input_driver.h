c6059291b70d182a46ee16aae53c390172075cea
Kona  ~  2025 © <168472716+Cat-Gawr@users.noreply.github.com>
2025-04-23 17:55:03 +0200

    Create nop_input_driver.h

---
+++ src/xenia/hid/nop2/nop_input_driver.h
@@ -0,0 +1,29 @@
+#ifndef XENIA_HID_NOP_NOP_INPUT_DRIVER_H_
+#define XENIA_HID_NOP_NOP_INPUT_DRIVER_H_
+
+#include "xenia/hid/input_driver.h"
+
+namespace xe {
+namespace hid {
+namespace nop {
+
+class NopInputDriver final : public InputDriver {
+ public:
+  explicit NopInputDriver(xe::ui::Window* window, size_t window_z_order);
+  ~NopInputDriver() override;
+
+  X_STATUS Setup() override;
+
+  X_RESULT GetCapabilities(uint32_t user_index, uint32_t flags,
+                           X_INPUT_CAPABILITIES* out_caps) override;
+  X_RESULT GetState(uint32_t user_index, X_INPUT_STATE* out_state) override;
+  X_RESULT SetState(uint32_t user_index, X_INPUT_VIBRATION* vibration) override;
+  X_RESULT GetKeystroke(uint32_t user_index, uint32_t flags,
+                        X_INPUT_KEYSTROKE* out_keystroke) override;
+};
+
+}  // namespace nop
+}  // namespace hid
+}  // namespace xe
+
+#endif  // XENIA_HID_NOP_NOP_INPUT_DRIVER_H_
