4088a9123aae4ff5cc01b4cbf8bc8c36d5cd73fb
Konata  ~  2025 © <168472716+Cat-Gawr@users.noreply.github.com>
2025-04-25 16:24:44 +0200

    Create entry_table.h

---
+++ src/xenia/cpu/entry_table.h
@@ -0,0 +1,47 @@
+#ifndef XENIA_CPU_ENTRY_TABLE_H_
+#define XENIA_CPU_ENTRY_TABLE_H_
+
+#include <unordered_map>
+#include <vector>
+
+#include "xenia/base/mutex.h"
+
+namespace xe {
+namespace cpu {
+
+class Function;
+
+typedef struct Entry_t {
+  typedef enum {
+    STATUS_NEW = 0,
+    STATUS_COMPILING,
+    STATUS_READY,
+    STATUS_FAILED,
+  } Status;
+
+  uint32_t address;
+  uint32_t end_address;
+  Status status;
+  Function* function;
+} Entry;
+
+class EntryTable {
+ public:
+  EntryTable();
+  ~EntryTable();
+
+  Entry* Get(uint32_t address);
+  Entry::Status GetOrCreate(uint32_t address, Entry** out_entry);
+
+  std::vector<Function*> FindWithAddress(uint32_t address);
+
+ private:
+  xe::global_critical_region global_critical_region_;
+  // TODO(benvanik): replace with a better data structure.
+  std::unordered_map<uint32_t, Entry*> map_;
+};
+
+}  // namespace cpu
+}  // namespace xe
+
+#endif  // XENIA_CPU_ENTRY_TABLE_H_
