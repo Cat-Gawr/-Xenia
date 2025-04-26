1bc0be2818302efeb774ca0ee2ed3764cd041cdf
Konata  ~  2025 © <168472716+Cat-Gawr@users.noreply.github.com>
2025-04-26 14:59:37 +0200

    Create kernel_flags.h

---
+++ src/xenia/kernel/kernel_flags.h
@@ -0,0 +1,9 @@
+
+#ifndef XENIA_KERNEL_KERNEL_FLAGS_H_
+#define XENIA_KERNEL_KERNEL_FLAGS_H_
+#include "xenia/base/cvar.h"
+
+DECLARE_bool(headless);
+DECLARE_bool(log_high_frequency_kernel_calls);
+
+#endif  // XENIA_KERNEL_KERNEL_FLAGS_H_
