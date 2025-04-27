6c7ca07df489053d2b69e948199068281889ca2f
Konata  ~  2025 © <168472716+Cat-Gawr@users.noreply.github.com>
2025-04-27 17:44:12 +0200

    Create compiler_pass.h

---
+++ src/xenia/cpu/compiler/compiler_pass.h
@@ -0,0 +1,41 @@
+
+#ifndef XENIA_CPU_COMPILER_COMPILER_PASS_H_
+#define XENIA_CPU_COMPILER_COMPILER_PASS_H_
+
+#include "xenia/base/arena.h"
+#include "xenia/cpu/hir/hir_builder.h"
+
+namespace xe {
+namespace cpu {
+class Processor;
+}  // namespace cpu
+}  // namespace xe
+
+namespace xe {
+namespace cpu {
+namespace compiler {
+
+class Compiler;
+
+class CompilerPass {
+ public:
+  CompilerPass();
+  virtual ~CompilerPass();
+
+  virtual bool Initialize(Compiler* compiler);
+
+  virtual bool Run(hir::HIRBuilder* builder) = 0;
+
+ protected:
+  Arena* scratch_arena() const;
+
+ protected:
+  Processor* processor_;
+  Compiler* compiler_;
+};
+
+}  // namespace compiler
+}  // namespace cpu
+}  // namespace xe
+
+#endif  // XENIA_CPU_COMPILER_COMPILER_PASS_H_
