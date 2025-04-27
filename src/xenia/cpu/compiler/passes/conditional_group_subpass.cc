2ea3e91f0c44e86380e1d6112b2296948ca885e3
Konata  ~  2025 © <168472716+Cat-Gawr@users.noreply.github.com>
2025-04-27 17:52:33 +0200

    Create conditional_group_subpass.cc

---
+++ src/xenia/cpu/compiler/passes/conditional_group_subpass.cc
@@ -0,0 +1,18 @@
+
+#include "xenia/cpu/compiler/passes/conditional_group_subpass.h"
+
+#include "xenia/cpu/compiler/compiler.h"
+
+namespace xe {
+namespace cpu {
+namespace compiler {
+namespace passes {
+
+ConditionalGroupSubpass::ConditionalGroupSubpass() : CompilerPass() {}
+
+ConditionalGroupSubpass::~ConditionalGroupSubpass() = default;
+
+}  // namespace passes
+}  // namespace compiler
+}  // namespace cpu
+}  // namespace xe
