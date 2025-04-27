e3f57fa83733c7729cf941ed7efc08df4dbb1efe
Konata  ~  2025 © <168472716+Cat-Gawr@users.noreply.github.com>
2025-04-27 17:47:45 +0200

    Create memory_sequence_combination_pass.h

---
+++ src/xenia/cpu/compiler/passes/memory_sequence_combination_pass.h
@@ -0,0 +1,29 @@
+#ifndef XENIA_CPU_COMPILER_PASSES_MEMORY_SEQUENCE_COMBINATION_PASS_H_
+#define XENIA_CPU_COMPILER_PASSES_MEMORY_SEQUENCE_COMBINATION_PASS_H_
+
+#include "xenia/cpu/compiler/compiler_pass.h"
+
+namespace xe {
+namespace cpu {
+namespace compiler {
+namespace passes {
+
+class MemorySequenceCombinationPass : public CompilerPass {
+ public:
+  MemorySequenceCombinationPass();
+  ~MemorySequenceCombinationPass() override;
+
+  bool Run(hir::HIRBuilder* builder) override;
+
+ private:
+  void CombineMemorySequences(hir::HIRBuilder* builder);
+  void CombineLoadSequence(hir::Instr* i);
+  void CombineStoreSequence(hir::Instr* i);
+};
+
+}  // namespace passes
+}  // namespace compiler
+}  // namespace cpu
+}  // namespace xe
+
+#endif  // XENIA_CPU_COMPILER_PASSES_MEMORY_SEQUENCE_COMBINATION_PASS_H_
