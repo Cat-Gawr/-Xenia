7b2129387cc614fb6aafe5738ab7dc90a081366a
Konata  ~  2025 © <168472716+Cat-Gawr@users.noreply.github.com>
2025-04-30 17:40:18 +0200

    Create diff.py

---
+++ tools/diff.py
@@ -0,0 +1,10 @@
+"""python diff.py file1 file2 diff"""
+
+import difflib
+import sys
+
+diff = difflib.unified_diff(
+    open(sys.argv[1], encoding='utf-8').readlines(),
+    open(sys.argv[2], encoding='utf-8').readlines())
+with open(sys.argv[3], 'w', encoding='utf-8') as f:
+    f.write(''.join(diff))
